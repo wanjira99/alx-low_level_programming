1743d708f2ac2dacc307744c2ada3cca840b3831
wanjira99 <essywanjira99@gmail.com>
2022-03-10 10:29:51 -0800

    Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- *main
+ *main - Entry point
  *
- * return:always(0)-success
+ * return:Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzle");
 	return (0);
